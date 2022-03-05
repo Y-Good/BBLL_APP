8c42bd03e461707d4043a8cf7b9ce164d3c4e3fd
Y-Good <1196148142@qq.com>
2022-03-05 17:20:26 +0800

    1

--- lib/pages/video_detail/controllers/video_detail_controller.dart
+++ lib/pages/video_detail/controllers/video_detail_controller.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:mvideo/config/http/request/comment/comment_request.dart';
 import 'package:mvideo/config/http/request/user/user_request.dart';
-import 'package:mvideo/config/public.dart';
 import 'package:mvideo/models/public.dart';
 import 'package:mvideo/utils/common/common_utils.dart';
 import 'package:mvideo/utils/user_utils.dart';
